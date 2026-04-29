ebf9c7b20aa04106feca85451c6a35948e0d7838
Mattiz Hoornaert <mattiz.hoornaert@gmail.com>
2026-04-29 21:51:12 +0200

    remove bundled demo media

--- src/utils/demoSongs.js
+++ src/utils/demoSongs.js
@@ -1,19 +1,10 @@
 import { createDefaultCoverDataUrl } from './defaultCover';
-import { DEMO_LIBRARY } from './demoMedia';
 
 export const DEMO_SONGS = [
   {
-    id: 'demo-song-4ad',
-    title: '4AD',
-    lyrics: `4AD\n\nDemo song van YOWLMAFFIA.\n\nPlay it loud.\n\n# Media\n${DEMO_LIBRARY[0].url}`,
-    cover_url: 'assets/yowl.jpg',
-    last_edited_by: 'YOWLMAFFIA',
-    updated_at: new Date().toISOString()
-  },
-  {
-    id: 'demo-song-chickie',
-    title: 'Ik e me chickie',
-    lyrics: `Ik e me chickie\n\nDemo song van YOWLMAFFIA.\n\n# Media\n${DEMO_LIBRARY[1].url}`,
+    id: 'demo-song-yowlmaffia',
+    title: 'YOWLMAFFIA',
+    lyrics: 'Demo song van YOWLMAFFIA.\n\nHier komen jullie echte songs uit Supabase binnen.',
     cover_url: 'assets/yowl.jpg',
     last_edited_by: 'YOWLMAFFIA',
     updated_at: new Date().toISOString()
